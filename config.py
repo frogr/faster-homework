20efe7b1b75ee46671509643dc3220c03368b449
frogr <austindanielfrench@gmail.com>
2018-05-19 19:06:10 -0700

    figured out how enums work in python

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ name3 = "cjmarsh725"
 name4 = "Davidless"
 name5 = "imsdhk"
 name6 = "Julian-Alexander"
-name8 = "Markstez05"
-name9 = "rhogan29"
+name7 = "Markstez05"
+name8 = "rhogan29"
+
+nameList = [name1, name2, name3, name4, name5, name6, name7, name8]
--- main.py
+++ main.py
@@ -10,13 +10,9 @@ id5 = input("Enter ID of the pull for %s: " % config.name5)
 id6 = input("Enter ID of the pull for %s: " % config.name6)
 id7 = input("Enter ID of the pull for %s: " % config.name7)
 id8 = input("Enter ID of the pull for %s: " % config.name8)
-print("\ncopy the entire block of text below and paste it into your terminal at the root repo\n")
-print("git fetch origin pull/"+id1+"/head:"+config.name1)
-print("git fetch origin pull/"+id2+"/head:"+config.name2)
-print("git fetch origin pull/"+id3+"/head:"+config.name3)
-print("git fetch origin pull/"+id4+"/head:"+config.name4)
-print("git fetch origin pull/"+id5+"/head:"+config.name5)
-print("git fetch origin pull/"+id6+"/head:"+config.name6)
-print("git fetch origin pull/"+id7+"/head:"+config.name7)
-print("git fetch origin pull/"+id8+"/head:"+config.name8)
+idList = [id1, id2, id3, id4, id5, id6, id7, id8]
 
+print("\ncopy the entire block of text below and paste it into your terminal at the root repo\n")
+
+for i, n in enumerate(config.nameList):
+        print("git fetch origin pull/"+idList[i]+"/head:"+n)
